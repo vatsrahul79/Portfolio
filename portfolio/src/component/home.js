426fef6e060be045b2e72e8ec8dbed6a275d1692
vatsrahul79 <vatsrahul79@gmail.com>
2019-12-27 22:07:39 +0530

    updated

--- portfolio/src/component/home.js
+++ portfolio/src/component/home.js
@@ -6,27 +6,27 @@ import React from 'react';
     class Home extends React.Component {
         render(){
             return(
-                <div className = "pl-3 pr-3">
+                <div className = " container  col-12 pl-3 pr-3">
                     <div className = "row">
-                        <div className = "col-lg-6 col-md-12 col-sm-12 jumbotron" style = {{backgroundColor:"#d621ad"}}>
-                            <img src = "123.JPG" className = "offset-3" style = {{borderRadius: "50%", border: "10px solid  #d61055", height : "150px",width:"150px"}}></img>
+                        <div className = "col-lg-6 col-md-12 col-sm-12 jumbotron" style = {{backgroundColor:" #e8f8f5 "}}>
+                            <img src = "123.JPG" className = "offset-3" style = {{borderRadius: "50%", border: "10px solid  #fdfefe", height : "150px",width:"150px"}}></img>
                                 <div className = "offset-5">
-                                    <h1 style = {{fontFamily: "Open Sans", fontSize: "38px"}} className = "text-light"><b>Rahul Kumar</b></h1>
-                                    <h1 style = {{fontFamily: "Open Sans", fontSize: "38px"}} className = "text-light"><b>Vats</b></h1>
+                                    <h1 style = {{fontFamily: "Open Sans", fontSize: "38px"}} className = "text-dark"><b>Rahul Kumar vats</b></h1>
+                                    {/* <h1 style = {{fontFamily: "Open Sans", fontSize: "38px"}} className = "text-dark"><b>Vats</b></h1> */}
                                     <h5><b>Full Stack Developer</b></h5>
                                     <div className = "row mt-3">
-                                        <img className  = "col-2 ml-n2" src = "loc.png" style = {{float: "left", height: "20px", width: "50px"}}></img>
+                                        <img className  = "col-2 ml-n2" src = "location.png" ></img>
                                         <h6 className = "ml-n3"> Bengaluru, Karnataka, India</h6>
                                     </div>
                                     <div className = "row mt-3">
-                                        <img className  = "col-2 ml-n2" src = "call.png" style = {{float: "left", height: "20px", width: "50px"}}></img>
+                                        <img className  = "col-2 ml-n2" src = "call.png" ></img>
                                         <h6 className = "ml-n3">8951519217</h6>
                                     </div>
                                     <div className = "row mt-3">
-                                        <img className  = "col-2 ml-n2" src = "mail.png" style = {{float: "left", height: "20px", width: "50px"}}></img>
+                                        <img className  = "col-2 ml-n2" src = "email.png" ></img>
                                         <h6 className = "ml-n3">vatsrahul79@gmail.com</h6>
                                     </div>
-                                    <h6 className = "mt-3 text-light">A passionate aspiring Full Stack Developer skilled in MERN stack, moulded and shaped by Masai School's numerous training. Actively ready to join in a great 
+                                    <h6 className = "mt-3 text-dark">A passionate aspiring Full Stack Developer skilled in MERN stack, moulded and shaped by Masai School's numerous training. Actively ready to join in a great 
                                     lively team of a good start-up to adapt me in any situation and environment with ease and perform the best.<a href="https://drive.google.com/file/d/1dzO_1O7BPHauyt-d8nPO_f-HPx4nV5gR/view?usp=sharing" target="blank">View Resume</a></h6>
                                 </div>
                                 <div className = "row mt-5">
@@ -39,48 +39,44 @@ import React from 'react';
                                     <div className = "col-1 ml-4">
                                        <a href ="https://github.com/vatsrahul79" target="blank"><img src = "https://image.flaticon.com/icons/svg/733/733609.svg" style = {{height: "50px", width:"50px"}}></img></a>
                                     </div>
-                                    {/* <div className = "col-1 ml-4">
-                                        <a href = "https://www.hackerrank.com/1"><img src = "https://brandfolder.com/hackerrank/logo/hackerrank-primary-logo.png" style = {{height: "50px", width:"50px"}}></img></a>
-                                    </div> */}
                                     <div className = "col-1 ml-4">
                                         <a href = "mailto:vatsrahul79@gmail.com"><img src = "https://icon-library.net/images/email-svg-icon/email-svg-icon-5.jpg" style = {{height: "50px", width:"50px"}}></img></a>
                                     </div>
                                 </div>  
                         </div>
-                        <div className = "col-lg-6 col-md-12 col-sm-12 jumbotron" style = {{ background: "linear-gradient(#d621ad, #2c3e50)"}}>
+                        <div className = "col-lg-6 col-md-12 col-sm-12 jumbotron" style = {{ background:"#d1f2eb "}}>
                             <div className = "row">
                                 <div className = "col-4 p-4">
-                                    <img src = "html.png" style = {{height: "110px", width: "120px", borderRadius: "50%" }}></img>
-                                    {/* <h3 className = "offset-2">HTML</h3> */}
+                                    <img src = "html.png" className='rounded-circle col-lg-12 col-md-12 col-sm-12'></img>
                                 </div>
                                 <div className = "col-4 p-4">
-                                    <img src = "css.png" style = {{height: "110px", width: "110px", borderRadius:"50%" }}></img>
-                                    {/* <h3 className = "offset-2">Css</h3> */}
+                                    <img src = "css.png" className='rounded-circle col-lg-12 col-md-12 col-sm-12'></img>
                                 </div>
                                 <div className = "col-4 p-4">
-                                    <img src = "js.png" style = {{height: "110px", width: "110px", borderRadius: "50%" }}></img>
-                                    {/* <h3 className = "ml-n3">JavaScript</h3> */}
+                                    <img src = "js.png"className='rounded-circle col-lg-12 col-md-12 col-sm-12'></img>
                                 </div>
                             </div>
                             <div className = "row">
                                 <div className = "col-4 p-4">
-                                    <img src = "bsp.png" style = {{height: "110px", width: "110px", borderRadius: "50%" }}></img>
-                                    {/* <h3>Bootstrap</h3> */}
+                                    <img src = "bsp.png" className='rounded-circle col-lg-12 col-md-12 col-sm-12'></img>
                                 </div>
                                 <div className = "col-4 p-4">
-                                    <img src = "react.png" style = {{height: "110px", width: "110px",borderRadius: "50%"  }}></img>
-                                    {/* <h3 className = "offset-2">React</h3> */}
+                                    <img src = "react.png" className='rounded-circle col-lg-12 col-md-12 col-sm-12'></img>
                                 </div>
                                 <div className = "col-4 p-4">
-                                    <img src = "favicon.ico" style = {{height: "110px", width: "110px", borderRadius: "50%" }}></img>
-                                    {/* <h3 className = "offset-1">Redux</h3> */}
+                                    <img src = "favicon.ico" className='rounded-circle col-lg-12 col-md-12 col-sm-12'></img>
                                 </div>
                             </div>
-                            <div className = "offset-3">
+                            <div className = "row">
                                     <div className = "col-4 p-4">
-                                        <img className = "offset-5" src = "python.png" style = {{height: "110px", width: "110px",borderRadius: "50%" }}></img>
-                                        {/* <h3 className = "offset-5">Python</h3> */}
+                                        <img  src = "python.png" className='rounded-circle col-lg-12 col-md-12 col-sm-12'></img>
                                     </div>
+                                    <div className = "col-4 p-4">
+                                    <img src = "flask.jpg" className='rounded-circle col-lg-12 col-md-12 col-sm-12'></img>
+                                    </div>
+                                    <div className = "col-4 p-4">
+                                    <img src = "mongo.png" className='rounded-circle col-lg-12 col-md-12 col-sm-12'></img>
+                                </div>
                             </div>
                         </div>
                     </div>
@@ -107,8 +103,8 @@ import React from 'react';
                     <div className = "jumbotron-fluid pl-2 pr-3 mt-5 mb-5">
                         <h1 className = "text-center">Projects</h1>
                         <CardGroup className = "row">
-                            <Card className = "col-lg-3 col-md-12 col-sm-12">
-                                <Card.Img variant="top" src="w1.jpeg"  style = {{height : "200px", width : "300px"}} />
+                            <Card className = "col-lg-12 w-100 col-md-12 col-sm-12">
+                                <Card.Img variant="top" src="w1.jpeg" style={{height:"300px"}} />
                                 <Card.Body>
                                 <Card.Title className = "text-center"><h2>Genie</h2></Card.Title>
                                 <Card.Text>
@@ -118,8 +114,8 @@ import React from 'react';
                                 <a href = "https://github.com/vatsrahul79/masai-week-1" className = "btn btn-outline-danger mb-5">View Project</a>
                             </Card>
                            
-                             <Card className = "col-lg-3 col-md-12 col-sm-12">
-                                <Card.Img variant="top" src="sat.jpg" style = {{height : "200px", width : "300px"}} />
+                             <Card className = "col-lg-12 w-100 col-md-12 col-sm-12">
+                                <Card.Img variant="top" src="sat.jpg" style = {{height : "300px"}} />
                                 <Card.Body>
                                 <Card.Title className = "text-center"><h2>Satellite</h2></Card.Title>
                                 <Card.Text>
@@ -128,8 +124,8 @@ import React from 'react';
                                 </Card.Body>
                                 <a href = "https://github.com/vatsrahul79/masai-week-2" className = "btn btn-outline-danger mb-5">View Project</a>
                             </Card>
-                            <Card className = "col-lg-3 col-md-12 col-sm-12">
-                                <Card.Img variant="top" src="w4.jpeg"  style = {{height : "200px", width : "300px"}} />
+                            <Card className = "col-lg-12 w-100 col-md-12 col-sm-12">
+                                <Card.Img variant="top" src="w4.jpeg" style={{height:"300px"}} />
                                 <Card.Body>
                                 <Card.Title className = "text-center"><h2>Bhagwatgita</h2></Card.Title>
                                 <Card.Text>
@@ -138,8 +134,8 @@ import React from 'react';
                                 </Card.Body>
                                 <a href = "https://github.com/vatsrahul79/Masai-Week-5/tree/master/week_5" className = "btn btn-outline-danger mb-5">View Project</a>
                             </Card>
-                            <Card className = "col-lg-3 col-md-12 col-sm-12">
-                                <Card.Img variant="top" src="w7.png"  style = {{height : "200px", width : "300px"}} />
+                            <Card className = "col-lg-12 w-100 col-md-12 col-sm-12">
+                                <Card.Img variant="top" src="w7.png" style={{height:"300px"}} />
                                 <Card.Body>
                                 <Card.Title className = "text-center"><h2>Microsoft</h2></Card.Title>
                                 <Card.Text>
@@ -149,47 +145,37 @@ import React from 'react';
                                 <a href = "https://github.com/vatsrahul79/Masai-Week-7/tree/master/week_7" className = "btn btn-outline-danger mb-5">View Project</a>
                             </Card>
                         </CardGroup>
-                        {/*<CardGroup className = "row">
-                            <Card className = "col-lg-4 col-md-12 col-sm-12">
-                                <Card.Img variant="top" src="w4.png" />
-                                <Card.Body>
-                                <Card.Title className = "text-center"><h2>Stock Manager</h2></Card.Title>
-                                <Card.Text>
-                                    Designed Dynamic Stock Manager using JavaScript
-                                </Card.Text>
-                                </Card.Body>
-                                <a href = "https://github.com/masai-week-4/tree/master/week_4" className = "btn btn-outline-danger mb-5">View Project</a>
-                                </CardGroup> */}
+                       
                     </div>
-                    <div className = "jumbptron-fluid pl-3 pr-3" style = {{ background: "linear-gradient(#d621ad, #2c3e50)"}}>
+                    <div className = "jumbptron-fluid pl-3 pr-3" style = {{backgroundColor:" #e8f8f5 "}}>
                         <div className = "row text-center pl-3">
                             <div className="col-lg-4 col-md-6 col-sm-12 mt-5">
-                                <div className="mt-5 offset-3" style={{width: "150px",height:"150px", background: "#C8F2EE", borderRadius: "50%", border:"20px solid", borderColor: "white white #d621ad #d621ad"}}>
+                                <div className="mt-5 offset-3" style={{width: "150px",height:"150px", background: "#C8F2EE", borderRadius: "50%", border:"20px solid #fdfefe"}}>
                                 <div className="text-center display-4 mt-4 font-weight-bold" style = {{marginTop: "60px",fontSize : "40px"}}>
-                                    600+
+                                    1000+
                                 </div>
                                 </div>
-                                <div className=" display-4 mt-2 font-weight-bold text-light" style= {{color:"#5D86B4", fontSize : "30px" }}>Hours of Full Stack Coding</div>
+                                <div className=" display-4 mt-2 font-weight-bold text-dark" style= {{color:"#5D86B4", fontSize : "30px" }}>Hours of Full Stack Coding</div>
                             </div> 
                             <div className = "col-lg-4 col-md-6 col-sm-12 mt-5">
-                                <div className="mt-5 offset-3" style={{width: "150px",height:"150px", background: "#C8F2EE", borderRadius: "50%", border:"20px solid", borderColor: " #960526  #960526  white white"}}>
+                                <div className="mt-5 offset-3" style={{width: "150px",height:"150px", background: "#C8F2EE", borderRadius: "50%", border:"20px solid #fdfefe", }}>
                                 <div className="text-center display-4 mt-4 font-weight-bold" style = {{marginTop: "60px",fontSize : "40px"}}>
-                                    150+
+                                    180+
                                 </div>
                                 </div>
-                                <div className=" display-4 mt-2 font-weight-bold text-light" style= {{color:"#5D86B4", fontSize : "30px" }}>Hackerrank Problem</div>         
+                                <div className=" display-4 mt-2 font-weight-bold text-dark" style= {{color:"#5D86B4", fontSize : "30px" }}>Hackerrank Problem</div>         
                             </div>
                             <div className = 'col-lg-4 col-md-6 col-sm-12 mt-5'>
-                                <div className="mt-5 offset-3" style={{width: "150px",height:"150px", background: "#C8F2EE", borderRadius: "50%", border:"20px solid", borderColor: "white  #f7809b   #f7809b  white"}}>
+                                <div className="mt-5 offset-3" style={{width: "150px",height:"150px", background: "#C8F2EE", borderRadius: "50%", border:"20px solid #fdfefe", }}>
                                 <div className="text-center display-4 mt-4 font-weight-bold" style = {{marginTop: "60px",fontSize : "40px"}}>
-                                    150+
+                                    190+
                                 </div>
                                 </div>
-                                <div className="display-4 mt-2 ml-n5 font-weight-bold text-light" style= {{color:"#5D86B4", fontSize : "30px" }}>Git Commits</div>
+                                <div className="display-4 mt-2 ml-n5 font-weight-bold text-dark" style= {{color:"#5D86B4", fontSize : "30px" }}>Git Commits</div>
                             </div>
                         </div>
                         <center>
-                        <div className = "mt-5 text-light">
+                        <div className = "mt-5 text-dark">
                         <GitHubCalendar username="vatsrahul79" color="#d621ad"style={{ margin: "30px 0px" }}>
                             <ReactTooltip delayShow={50} html />
                         </GitHubCalendar>
