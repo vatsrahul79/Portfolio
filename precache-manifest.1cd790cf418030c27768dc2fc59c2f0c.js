24095fc28f29a4b51bea1eb4f1657a673e1fb234
vatsrahul79 <vatsrahul79@gmail.com>
2020-11-03 18:04:10 +0530

    Updates

---
+++ precache-manifest.1cd790cf418030c27768dc2fc59c2f0c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fa84849d2f9df05213e382acfde1fa26",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "903e33b11d8387829b81",
+    "url": "/portfolio/static/css/main.f773e65a.chunk.css"
+  },
+  {
+    "revision": "d2ca768432c4aa843dc1",
+    "url": "/portfolio/static/js/2.89ce55e6.chunk.js"
+  },
+  {
+    "revision": "903e33b11d8387829b81",
+    "url": "/portfolio/static/js/main.61ab7399.chunk.js"
+  },
+  {
+    "revision": "76aa8cbd67b5c937ec2c",
+    "url": "/portfolio/static/js/runtime-main.e92c632a.js"
+  }
+]);
